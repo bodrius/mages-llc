5ed4b3c81eee269d5431183f1d8da4b13514c72b
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-06-01 16:21:36 +0300

    fix php

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './scss/main.scss';
 import "./scss/section/header";
 import "./scss/section/galery";
 import "./scss/section/form";
-// import "./scss/section/email";
+import "./scss/section/ajax";
---
+++ src/scss/section/ajax.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  $('.your-class').slick(
+    {
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000	
+    }
+);
+
+
+
+$('form').on('submit', (function (e) {
+  e.preventDefault();
+  var form = $(this);
+  var error = false;
+  
+  form.find('textaria, input').each(function () {
+    if ($(this).val() == '') {
+    error = true;
+    }
+  });
+
+  if (!error) {
+    var data = form.serialize();
+    $.ajax({
+    type: 'POST',
+    url: './email.php',
+    dataType: 'json',
+    data: data,
+    beforeSend: function beforeSend(data) {
+      form.find('.contact__button').attr('disabled', 'disabled');
+    },
+    success: function(response) {
+      alert('Спасибо за ваш заказ!');
+      // window.location.href = "./success/index.html"
+      //window.open("http://http://1652079.smokerst.web.hosting-test.net/index2.html");
+    },
+    complete: function complete(data) {
+      form.find('.contact__button').prop('disabled', false);
+    }
+    });
+  }
+
+  return false;
+  }));
+});
